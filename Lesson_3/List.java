df650e45797098e35598b291ff6e3a9f7b08304d
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-07-11 23:55:51 +0400

    Lesson 3, singly linked list revert w/o tail

--- Lesson_3/List.java
+++ Lesson_3/List.java
@@ -6,73 +6,16 @@ package Lesson_3;
  */
 public class List {
     Node head;
-    Node tail;
 
-    public void add(int value) {
-        Node node = new Node();
-        node.value = value;
-        if (head == null) {
-            head = node;
-            tail = node;
-        } else {
-            tail.next = node;
-            node.previous = tail;
-            tail = node;
-        }
+    public void revertR() {
+        if (head == null && head.next != null) revertR(head.next, head);
     }
-
-    public void add(int value, Node node) {
-        Node next = node.next;
-        Node currentNode = new Node();
-        currentNode.value = value;
-        node.next = currentNode;
-        currentNode.previous = node;
-        if (next == null) tail = currentNode;
-        else {
-            next.previous = currentNode;
-            currentNode.next = next;
-        }
+    private void revertR(Node current, Node previous) {
+        if (current.next == null) head = current;
+        else revertR(current.next, current)
+        current.next = previous;
+        previous.next = null;
     }
-
-    public void del(Node node) {
-        Node previous = node.previous;
-        Node next = node.next;
-        if (previous == null) {
-            next.previous = null;
-            head = next;
-        } else {
-            if (next == null) {
-                previous.next = null;
-                tail = previous;
-            } else {
-                previous.next = next;
-                next.previous = previous;
-            }
-        }
-    }
-
-    public void revert() {
-        Node currentNode = head;
-        while (currentNode != null) {
-            Node next = currentNode.next;
-            Node previous = currentNode.previous;
-            currentNode.previous = next;
-            currentNode.next = previous;
-            if (previous == null) tail = currentNode;
-            if (next == null) head = currentNode;
-            currentNode = next;
-        }
-    }
-
-    public Node find(int value) {
-        Node currentNode = head;
-        while (currentNode != null) {
-            if (currentNode.value == value) return currentNode;
-            currentNode = currentNode.next;
-        }
-        return null;
-    }
-
     /**
      * Node
      */
@@ -80,6 +23,5 @@ public class List {
     
         int value;
         Node next;
-        Node previous;
     }
 }
